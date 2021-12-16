faedfc48bf168184e69272b6e79e2c0f87bf06e2
jdragon <1061917196@qq.com>
2021-12-16 23:41:47 +0800

    test

--- src/main/java/org/example/redis/SpringRedisMq.java
+++ src/main/java/org/example/redis/SpringRedisMq.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class App {
+public class SpringRedisMq {
     public static void main(String[] args) {
-        SpringApplication.run(App.class);
+        SpringApplication.run(SpringRedisMq.class);
     }
 }
